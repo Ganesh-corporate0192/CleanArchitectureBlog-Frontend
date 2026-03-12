ac605af22baffa7d62385234d65ba10358eb9408
Ganesh-corporate0192 <EMAIL_USED_IN_GITHUB_REPO>
2026-03-12 18:08:05 +0530

    Edit single blog is removed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { BlogListComponent } from './components/blog-list/blog-list.component';
 import { BlogCreateComponent } from './components/blog-create/blog-create.component';
-import { BlogEditComponent } from './components/blog-edit/blog-edit.component';
 import { BlogListResolver } from './resolvers/blog-list.resolver';
 
 export const routes: Routes = [
@@ -14,8 +13,5 @@ export const routes: Routes = [
     path: 'create',
     component: BlogCreateComponent
   },
-  {
-    path: 'edit/:id',
-    component: BlogEditComponent
-  }
+
 ];
